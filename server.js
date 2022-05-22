7d2461a22bf9ec7efba97800a50220a607a1a3d7
Lucas Alves <lalves@korewireless.com>
2022-05-21 23:03:32 -0300

    adiconando um middleware de erro e adaptação do socket dos devices

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const server = require("./socket")
 
 const PORTA = process.env.PORT || 3333;
 
+server.use((error, req, res, next) => {
+    console.log('Erro não tratado', erro);
+    res.sendStatus(500);
+})
+
 server.listen(PORTA, function () {
     console.log(`Servidor do site está rodando rodando: ${process.env.APP_URL} \n
     Você está rodando sua aplicação em ${process.env.AMBIENTE_PROCESSO} \n
