cab3fea0bfa0dbdab1d956c29381f28894cd4852
Guilherme Henrique dos Santos <89214111+Gui-henrique-dos-santos@users.noreply.github.com>
2022-05-22 19:48:15 -0300

    funcionamento dos 3 graficos

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -27,15 +27,6 @@ router.delete("/:idUsuario", function (req, res) {
     usuarioController.deletar(req, res)
 })
 
-router.get("/dadosgraph1", function (req, res) {
-    usuarioController.graficoCpu(req, res);
-});
-router.get("/dadosgraph2", function (req, res) {
-    usuarioController.graficoDisco(req, res);
-});
-router.get("/dadosgraph3", function (req, res) {
-    usuarioController.graficoRam(req, res);
-});
 
 
 module.exports = router;
