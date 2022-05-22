321759cd62dc5c3de09797b45c55ffbb3ff9df35
Lucas Feitosa Alves <59104855+LucasTI79@users.noreply.github.com>
2022-05-22 14:06:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,5 +18,10 @@ app.use(cors());
 
 app.use(require('./src/routes'));
 
+app.use((error, req, res, next) => {
+    console.log('Erro não tratado', erro);
+    res.sendStatus(500);
+})
+
 module.exports = app;
 
