1e2bd57d2b436c7ef8875698fddfe2b45aa62e2b
Guilherme Henrique dos Santos <89214111+Gui-henrique-dos-santos@users.noreply.github.com>
2022-05-25 18:10:12 -0300

    atualização de grafico
    tirei o espelhamento

--- public/js/computador.js
+++ public/js/computador.js
@@ -1,20 +1,23 @@
 function cadastrarComputador() {
 
-    var patrimonio = iptPatrimonio.value;
-    var hostname = iptHostname.value;
+    const patrimonioServer = iptPatrimonio.value;
+    const hostnameServer = iptHostname.value;
+    const salaServer = iptSala.value;
 
-    if (patrimonio == "") {
+
+    if (patrimonioServer == "" || hostnameServer == "" || salaServer == "") {
         alert("Insira seus dados!")
     }
 
-    fetch('/usuarios/cadastrar', {
+    fetch('/instituicao/cadastrar', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            patrimonioServer: patrimonio,
-            hostnameServer: hostname
+            patrimonioServer,
+            hostnameServer,
+            salaServer
         })
     }).then(function (resposta) {
         let button = document.querySelector(".cadastrar");
@@ -22,14 +25,101 @@ function cadastrarComputador() {
         console.log("resposta: ", resposta);
 
         if (resposta.ok) {
-            window.alert("Cadastro da sala realizado com sucesso!");
+            window.alert("Cadastro do computador realizado com sucesso!");
             button.disabled = true;
         } else {
-            throw ("Houve um erro ao tentar realizar o cadastro da sala!");
+            throw ("Houve um erro ao tentar realizar o cadastro do computador!");
         }
     }).catch(function (resposta) {
         console.log(`#ERRO: ${resposta}`);
     });
 
     return false;   
+}
+function alterarMaquina() {
+    const patrimonioNovoServer = iptPatrimonioNovo.value;
+    const hostnameNovoServer = iptHostnameNovo.value;
+    const salaNovaServer = iptSalaNova.value;
+
+
+    fetch("/instituicao/alterarMaquina/" + sessionStorage.ID_COMPUTADOR, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            patrimonioNovoServer,
+            hostnameNovoServer,
+            salaNovaServer
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO atualizar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+
+                alert("Computador alterado com sucesso!")
+
+
+            });
+
+        } else {
+
+            console.log("Houve um erro ao tentar alterar a Computador");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                // finalizarAguardar(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+
+}
+
+function deletarComputador() {
+
+    fetch("/instituicao/" + sessionStorage.ID_COMPUTADOR, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO deletar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+
+                alert("Computador deletado com sucesso! ")
+
+
+            });
+
+        } else {
+
+            console.log("Houve um erro ao tentar deletar o computador");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                // finalizarAguardar(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
 }
